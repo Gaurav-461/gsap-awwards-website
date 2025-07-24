5cb3530663684e82112ed3b57e8b5b34dac65193
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-24 14:20:50 +0530

    Added Hero section and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-50 p-3 md:p-9">
+      <img src="/images/nav-logo.svg" alt="nav-logo" />
+    </nav>
+  )
+}
+
+export default Navbar
