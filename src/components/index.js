ee9ad8de52e663cfeabfbc3c8136a71c269d60df
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-29 03:00:22 +0530

    added index files

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import ClipPathTitle from "./ClipPathTitle";
+import FlavorSlide from "./FlavorSlide";
+import FlavorTitle from "./FlavorTitle";
+import Navbar from "./Navbar";
+import VideoPinSection from "./VideoPinSection";
+
+
+export {
+    Navbar,
+    FlavorSlide,
+    FlavorTitle,
+    ClipPathTitle,
+    VideoPinSection
+}
---
+++ src/sections/index.js
@@ -0,0 +1,9 @@
+import HeroSection from "./HeroSection";
+import MessageSection from "./MessageSection";
+import FlavorSection from "./FlavorSection";
+import NutritionSection from "./NutritionSection";
+import BenefitSection from "./BenefitSection";
+import TestimonialSection from "./TestimonialSection";
+import FooterSection from "./FooterSection";
+
+export { HeroSection, MessageSection, FlavorSection, NutritionSection, BenefitSection, TestimonialSection, FooterSection }
