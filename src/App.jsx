412e7ac8c12d673c1cb3d0504ccbc151ee1ce15a
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-29 03:01:47 +0530

    added all the sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { Navbar } from "./components/index";
+import {
+    BenefitSection,
+    FlavorSection,
+    FooterSection,
+    HeroSection,
+    MessageSection,
+    NutritionSection,
+    TestimonialSection,
+} from "./sections/index";
+import { ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import CustomCursor from "./components/customCursor";
+
+gsap.registerPlugin(SplitText, ScrollSmoother, ScrollTrigger, useGSAP);
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    useGSAP(() => {
+        ScrollSmoother.create({
+            smooth: 3,
+            effects: true,
+        });
+    });
+    return (
+        <main>
+            <CustomCursor />
+            <Navbar />
+            <div id="smooth-wrapper">
+                <div id="smooth-content">
+                    <HeroSection />
+                    <MessageSection />
+                    <FlavorSection />
+                    <NutritionSection />
 
-export default App
+                    <div>
+                        <BenefitSection />
+                        <TestimonialSection />
+                    </div>
+                    <FooterSection />
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default App;
