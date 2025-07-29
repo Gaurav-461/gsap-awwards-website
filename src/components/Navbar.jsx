7026dffc76761321dd842d5c840984662900897b
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-29 21:04:37 +0530

    updated components and added images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,76 @@
+import { useState } from "react";
 
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { menuData } from "../constants";
 
 const Navbar = () => {
-  return (
-    <nav className="fixed top-0 left-0 z-50 p-3 md:p-9">
-      <img src="/images/nav-logo.svg" alt="nav-logo" />
-    </nav>
-  )
-}
+    const [isOpen, setIsOpen] = useState(false);
+    const [image, setImage] = useState("/images/menu-img-1.webp");
 
-export default Navbar
+    const { contextSafe } = useGSAP(() => {
+      gsap.from(".menu-img", {
+        // opacity: 0,
+        // scale: 105,
+        duration: 1,
+        transform:"all"
+      })
+    },[image]);
+
+    const openMenu = contextSafe(() => {
+        setIsOpen(true);
+        gsap.to(".menu-container", {
+            height: "100vh",
+            ease: "slow",
+            duration: 1.5,
+        });
+    });
+    
+    const closeMenu = contextSafe(() => {
+        setIsOpen(false);
+        gsap.to(".menu-container", {
+            height: "0vh",
+            duration: 1.5,
+            ease: "circ.inOut",
+        });
+    });
+
+    return (
+        <nav className="fixed top-0 left-0 z-[99999] w-[49.3%] p-3 md:p-9">
+            <div id="menu-icon" className="absolute z-100 flex w-full items-center justify-between will-change-transform">
+                <img src="/images/nav-logo.svg" alt="nav-logo" />
+                {isOpen ? (
+                    <button onClick={closeMenu} className="bg-milk-yellow p-2 rounded-full">Close</button>
+                ) : (
+                    <button onClick={openMenu} className="bg-milk-yellow p-2 rounded-full">Menu</button>
+                )}
+            </div>
+            <div className="menu-container absolute top-0 left-0 z-50 flex h-[0vh] w-dvw overflow-hidden">
+                <div className="left-side flex h-full w-1/2 items-center justify-center bg-milk">
+                    <div className="item-list flex flex-col items-center">
+                        {menuData.map(({ label, img }) => (
+                            <h2
+                                onMouseMove={() => setImage(img)}
+                                onMouseLeave={() => setImage("/images/menu-img-1.webp")}
+                                className="item will-change-auto cursor-pointer text-center text-[12vh]/[105%] font-bold w-fit"
+                            >
+                                {label}
+                            </h2>
+                        ))}
+                    </div>
+                </div>
+
+                {/* img */}
+                <div className="right-side h-full w-1/2 bg-milk">
+                    <img
+                        src={image}
+                        alt="menu-img"
+                        className="menu-img size-full object-cover"
+                    />
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,92 +1,119 @@
 const flavorLists = [
-  {
-    name: "Chocolate Milk",
-    color: "brown",
-    rotation: "md:rotate-[-8deg] rotate-0",
-  },
-  {
-    name: "Stawberry Milk",
-    color: "red",
-    rotation: "md:rotate-[8deg] rotate-0",
-  },
-  {
-    name: "Cookies & Cream",
-    color: "blue",
-    rotation: "md:rotate-[-8deg] rotate-0",
-  },
-  {
-    name: "Peanut Butter Chocolate",
-    color: "orange",
-    rotation: "md:rotate-[8deg] rotate-0",
-  },
-  {
-    name: "Vanilla Milkshake",
-    color: "white",
-    rotation: "md:rotate-[-8deg] rotate-0",
-  },
-  {
-    name: "Max Chocolate Milk",
-    color: "black",
-    rotation: "md:rotate-[8deg] rotate-0",
-  },
+    {
+        name: "Chocolate Milk",
+        color: "brown",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "Stawberry Milk",
+        color: "red",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
+    {
+        name: "Cookies & Cream",
+        color: "blue",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "Peanut Butter Chocolate",
+        color: "orange",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
+    {
+        name: "Vanilla Milkshake",
+        color: "white",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "Max Chocolate Milk",
+        color: "black",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
 ];
 
 const nutrientLists = [
-  { label: "Potassium", amount: "245mg" },
-  { label: "Calcium", amount: "500mg" },
-  { label: "Vitamin A", amount: "176mcg" },
-  { label: "Vitamin D", amount: "5mcg" },
-  { label: "Iron", amount: "1mg" },
+    { label: "Potassium", amount: "245mg" },
+    { label: "Calcium", amount: "500mg" },
+    { label: "Vitamin A", amount: "176mcg" },
+    { label: "Vitamin D", amount: "5mcg" },
+    { label: "Iron", amount: "1mg" },
 ];
 
 const cards = [
-  {
-    src: "/videos/f1.mp4",
-    rotation: "rotate-z-[-10deg]",
-    name: "Madison",
-    img: "/images/p1.png",
-    translation: "translate-y-[-5%]",
-  },
-  {
-    src: "/videos/f2.mp4",
-    rotation: "rotate-z-[4deg]",
-    name: "Alexander",
-    img: "/images/p2.png",
-  },
-  {
-    src: "/videos/f3.mp4",
-    rotation: "rotate-z-[-4deg]",
-    name: "Andrew",
-    img: "/images/p3.png",
-    translation: "translate-y-[-5%]",
-  },
-  {
-    src: "/videos/f4.mp4",
-    rotation: "rotate-z-[4deg]",
-    name: "Bryan",
-    img: "/images/p4.png",
-    translation: "translate-y-[5%]",
-  },
-  {
-    src: "/videos/f5.mp4",
-    rotation: "rotate-z-[-10deg]",
-    name: "Chris",
-    img: "/images/p5.png",
-  },
-  {
-    src: "/videos/f6.mp4",
-    rotation: "rotate-z-[4deg]",
-    name: "Devante",
-    img: "/images/p6.png",
-    translation: "translate-y-[5%]",
-  },
-  {
-    src: "/videos/f7.mp4",
-    rotation: "rotate-z-[-3deg]",
-    name: "Melisa",
-    img: "/images/p7.png",
-    translation: "translate-y-[10%]",
-  },
+    {
+        src: "/videos/f1.mp4",
+        rotation: "rotate-z-[-10deg]",
+        name: "Madison",
+        img: "/images/p1.png",
+        translation: "translate-y-[-5%]",
+    },
+    {
+        src: "/videos/f2.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Alexander",
+        img: "/images/p2.png",
+    },
+    {
+        src: "/videos/f3.mp4",
+        rotation: "rotate-z-[-4deg]",
+        name: "Andrew",
+        img: "/images/p3.png",
+        translation: "translate-y-[-5%]",
+    },
+    {
+        src: "/videos/f4.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Bryan",
+        img: "/images/p4.png",
+        translation: "translate-y-[5%]",
+    },
+    {
+        src: "/videos/f5.mp4",
+        rotation: "rotate-z-[-10deg]",
+        name: "Chris",
+        img: "/images/p5.png",
+    },
+    {
+        src: "/videos/f6.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Devante",
+        img: "/images/p6.png",
+        translation: "translate-y-[5%]",
+    },
+    {
+        src: "/videos/f7.mp4",
+        rotation: "rotate-z-[-3deg]",
+        name: "Melisa",
+        img: "/images/p7.png",
+        translation: "translate-y-[10%]",
+    },
 ];
 
-export { flavorLists, nutrientLists, cards };
+const menuData = [
+    {
+        label: "SHOP",
+        img: "/images/menu-img-2.webp",
+    },
+    {
+        label: "FIND IN STORES",
+        img: "/images/menu-img-3.webp",
+    },
+    {
+        label: "ABOUT US",
+        img: "/images/menu-img-4.webp",
+    },
+    {
+        label: "TASTY TALKS",
+        img: "/images/menu-img-5.webp",
+    },
+    {
+        label: "PROGRAMS",
+        img: "/images/menu-img-7.webp",
+    },
+    {
+        label: "CONTACTS",
+        img: "/images/menu-img-6.webp",
+    },
+];
+
+export { flavorLists, nutrientLists, cards, menuData };
